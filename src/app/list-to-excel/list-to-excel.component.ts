6d135a349d6ffde3502fc1205a0c43e91cea5892
h4syy <offh4sy@gmail.com>
2022-07-23 13:09:19 +0545

    used services for data injection

--- src/app/list-to-excel/list-to-excel.component.ts
+++ src/app/list-to-excel/list-to-excel.component.ts
@@ -8,74 +8,34 @@ import { GroupstudentService } from '../services/groupstudent.service';
   styleUrls: ['./list-to-excel.component.scss'],
 })
 export class ListToExcelComponent implements OnInit {
-  dataset = [
-    {
-      id: '1',
-      Name: 'Reece Kay',
-      phone: '078 8646 0139',
-      email: 'ReeceKay@rhyta.com',
-    },
-    {
-      id: '2',
-      Name: 'Ellie Palmer',
-      phone: '078 6097 0693',
-      email: 'ElliePalmer@einrot.com',
-    },
-    {
-      id: '3',
-      Name: 'Mia Parker',
-      phone: '077 5067 2982',
-      email: 'MiaParker@cuvox.de',
-    },
-    {
-      id: '4',
-      Name: 'Louis Vincent',
-      phone: '079 8748 6044',
-      email: 'LouisVincent@teleworm.us',
-    },
-    {
-      id: '5',
-      Name: 'Ryan Parsons',
-      phone: '078 4877 0816',
-      email: 'RyanParsons@armyspy.com',
-    },
-    {
-      id: '6',
-      Name: 'Hayden Roberts',
-      phone: '077 0143 1358',
-      email: 'HaydenRoberts@superrito.com',
-    },
-    {
-      id: '7',
-      Name: 'Lola Lambert',
-      phone: '070 5368 6843',
-      email: 'LolaLambert@dayrep.com',
-    },
-  ];
-  data: [] = [];
+  data: any[] = [];
   constructor(
     private data1: GroupstudentService,
     private filerSaver: FileSaverService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.data = this.data1.getData();
+  }
   excelExport() {
     //code given by xlsx
     const EXCEL_TYPE =
       'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
-    const EXCEL_EXTENSION = '.xlsx';
-
-    //own code
+    const EXCEL_EXTENSION = '.xlsx'; //own code
     //creating a worksheet
-    const worksheet = XLSX.utils.json_to_sheet(this.dataset);
-    const workBook = {
-      Sheets: { testingSheet: worksheet },
-      SheetNames: ['testingSheet'],
-    };
+    const worksheet = XLSX.utils.json_to_sheet(this.data);
+    const workBook = XLSX.utils.book_new();
     const excelBuffer = XLSX.write(workBook, {
       bookType: 'xlsx',
       type: 'array',
     });
+
+    this.data.forEach((obj: any) => {
+      workBook.SheetNames.push(obj.group);
+      XLSX.utils.book_append_sheet(workBook, worksheet, obj.group);
+      console.log(workBook.SheetNames);
+    });
+
     //making a blobData of EXCEL TYPE
     const blobData = new Blob([excelBuffer], { type: EXCEL_TYPE });
     this.filerSaver.save(blobData, 'demofile');
--- src/app/services/groupstudent.service.ts
+++ src/app/services/groupstudent.service.ts
@@ -6,47 +6,203 @@ import { Injectable } from '@angular/core';
 export class GroupstudentService {
   private dataset = [
     {
-      id: '1',
-      Name: 'Reece Kay',
-      phone: '078 8646 0139',
-      email: 'ReeceKay@rhyta.com',
+      group: 'L1C1',
+      studentsList: [
+        {
+          id: 1,
+          fullname: 'ABC',
+          phone: '9090909090',
+          email: 'fdsbahfj@gmail.com',
+        },
+        {
+          id: 1,
+          fullname: 'ABC',
+          phone: '9090909090',
+          email: 'fdsbahfj@gmail.com',
+        },
+        {
+          id: 1,
+          fullname: 'ABC',
+          phone: '9090909090',
+          email: 'fdsbahfj@gmail.com',
+        },
+        {
+          id: 1,
+          fullname: 'ABC',
+          phone: '9090909090',
+          email: 'fdsbahfj@gmail.com',
+        },
+        {
+          id: 1,
+          fullname: 'ABC',
+          phone: '9090909090',
+          email: 'fdsbahfj@gmail.com',
+        },
+        {
+          id: 1,
+          fullname: 'ABC',
+          phone: '9090909090',
+          email: 'fdsbahfj@gmail.com',
+        },
+        {
+          id: 1,
+          fullname: 'ABC',
+          phone: '9090909090',
+          email: 'fdsbahfj@gmail.com',
+        },
+        {
+          id: 1,
+          fullname: 'ABC',
+          phone: '9090909090',
+          email: 'fdsbahfj@gmail.com',
+        },
+        {
+          id: 1,
+          fullname: 'ABC',
+          phone: '9090909090',
+          email: 'fdsbahfj@gmail.com',
+        },
+        {
+          id: 1,
+          fullname: 'ABC',
+          phone: '9090909090',
+          email: 'fdsbahfj@gmail.com',
+        },
+      ], // 10 items
     },
     {
-      id: '2',
-      Name: 'Ellie Palmer',
-      phone: '078 6097 0693',
-      email: 'ElliePalmer@einrot.com',
+      group: 'L1C2',
+      studentsList: [
+        {
+          id: 1,
+          fullname: 'ASD',
+          phone: '123845784948',
+          email: 'iisauif@gmail.com',
+        },
+        {
+          id: 1,
+          fullname: 'ASD',
+          phone: '123845784948',
+          email: 'iisauif@gmail.com',
+        },
+        {
+          id: 1,
+          fullname: 'ASD',
+          phone: '123845784948',
+          email: 'iisauif@gmail.com',
+        },
+        {
+          id: 1,
+          fullname: 'ASD',
+          phone: '123845784948',
+          email: 'iisauif@gmail.com',
+        },
+        {
+          id: 1,
+          fullname: 'ASD',
+          phone: '123845784948',
+          email: 'iisauif@gmail.com',
+        },
+        {
+          id: 1,
+          fullname: 'ASD',
+          phone: '123845784948',
+          email: 'iisauif@gmail.com',
+        },
+        {
+          id: 1,
+          fullname: 'ASD',
+          phone: '123845784948',
+          email: 'iisauif@gmail.com',
+        },
+        {
+          id: 1,
+          fullname: 'ASD',
+          phone: '123845784948',
+          email: 'iisauif@gmail.com',
+        },
+        {
+          id: 1,
+          fullname: 'ASD',
+          phone: '123845784948',
+          email: 'iisauif@gmail.com',
+        },
+        {
+          id: 1,
+          fullname: 'ASD',
+          phone: '123845784948',
+          email: 'iisauif@gmail.com',
+        },
+      ],
     },
     {
-      id: '3',
-      Name: 'Mia Parker',
-      phone: '077 5067 2982',
-      email: 'MiaParker@cuvox.de',
-    },
-    {
-      id: '4',
-      Name: 'Louis Vincent',
-      phone: '079 8748 6044',
-      email: 'LouisVincent@teleworm.us',
-    },
-    {
-      id: '5',
-      Name: 'Ryan Parsons',
-      phone: '078 4877 0816',
-      email: 'RyanParsons@armyspy.com',
-    },
-    {
-      id: '',
-      Name: 'Hayden Roberts',
-      phone: '077 0143 1358',
-      email: 'HaydenRoberts@superrito.com',
-    },
-    {
-      id: '',
-      Name: 'Lola Lambert',
-      phone: '070 5368 6843',
-      email: 'LolaLambert@dayrep.com',
+      group: 'L1C3',
+      studentsList: [
+        {
+          id: 1,
+          fullname: 'IUY',
+          phone: '128348682399',
+          email: 'offh4sy@gmail.com',
+        },
+        {
+          id: 1,
+          fullname: 'IUY',
+          phone: '128348682399',
+          email: 'offh4sy@gmail.com',
+        },
+        {
+          id: 1,
+          fullname: 'IUY',
+          phone: '128348682399',
+          email: 'offh4sy@gmail.com',
+        },
+        {
+          id: 1,
+          fullname: 'IUY',
+          phone: '128348682399',
+          email: 'offh4sy@gmail.com',
+        },
+        {
+          id: 1,
+          fullname: 'IUY',
+          phone: '128348682399',
+          email: 'offh4sy@gmail.com',
+        },
+        {
+          id: 1,
+          fullname: 'IUY',
+          phone: '128348682399',
+          email: 'offh4sy@gmail.com',
+        },
+        {
+          id: 1,
+          fullname: 'IUY',
+          phone: '128348682399',
+          email: 'offh4sy@gmail.com',
+        },
+        {
+          id: 1,
+          fullname: 'IUY',
+          phone: '128348682399',
+          email: 'offh4sy@gmail.com',
+        },
+        {
+          id: 1,
+          fullname: 'IUY',
+          phone: '128348682399',
+          email: 'offh4sy@gmail.com',
+        },
+        {
+          id: 1,
+          fullname: 'IUY',
+          phone: '128348682399',
+          email: 'offh4sy@gmail.com',
+        },
+      ],
     },
   ];
+  getData() {
+    return this.dataset;
+  }
   constructor() {}
 }
